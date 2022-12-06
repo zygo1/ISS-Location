025bc8be89280dffd615394b0236dd50f5b5fb4e
zygo1 <dimitrismx96@hotmail.com>
2022-12-06 13:06:38 +0200

    languages

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-//Create map and tiles
-const map = L.map('map').setView([0, 0], 2); //.setView([latitude, longitude],zoom)
-const attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
-const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-const tiles = L.tileLayer(tileUrl, { attribution });
-tiles.addTo(map);
+"use strict"
+//init map
+let map = L.map('map').setView([0, 0], 2); //.setView([latitude, longitude],zoom)
+let attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+let attributionSat = 'i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community';
+
+//Create map tiles
+let tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 
 
-//Satellite----------------
-var mapLink = '<a href="http://www.esri.com/">Esri</a>';
-var wholink = 'i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community';
-
+//Create satellite tiles
+let mapLink = '<a href="http://www.esri.com/">Esri</a>';
+let tileUrlSat = 'http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}';
 L.tileLayer(
-    'http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
-    attribution: '&copy; ' + mapLink + ', ' + wholink,
+    tileUrlSat, {
+    attribution: '&copy; ' + mapLink + ', ' + attributionSat,
     maxZoom: 20,
 }).addTo(map);
-
 
 //Set custom icon to the marker
 const myIcon = L.icon({
@@ -24,45 +24,53 @@ const myIcon = L.icon({
     iconAnchor: [25, 16],
 });
 
-//Reset marker to 0,0
+//Reset marker to 0,0 
 const marker = L.marker([0, 0], { icon: myIcon }).addTo(map);
 
-
-const api_url = "https://api.wheretheiss.at/v1/satellites/25544";
-const velocityData = [];
-let sum = 0;
+//Retrieve data from JSON
+let url = './language/gr.json'
 
 
-async function getISS() {
-    const response = await fetch(api_url);
-    const data = await response.json();
-    const { latitude, longitude, altitude, velocity, visibility } = data;
-    sum += velocity;
-    velocityData.push(velocity);
-    marker.setLatLng([latitude, longitude]);
-    locationISS(latitude, longitude, visibility);
-    printAttributes(velocityData, latitude, longitude, altitude, velocity, sum);
-    setTimeout(getISS, 4000);
-}
+function printAttributes(velocityData, lat, long, alt, velocity, sum, visibilityValue) {
+    //Labels
+    //  //TODO: add these to html doc
+    // let getLangData = async function (url) {
+    //     let response = await fetch(url);
+    //     let data = response.json();
+    //     let { latitude, longitude, altitude, approximatePos, visibility, speed } = data;
+    // };
 
-function locationISS(latitude, longitude, visibility) {
-    if (latitude > 1 && longitude < 0) {
+    // console.log(data)
+    // let myVariable = "" + data + longitude;
+
+    // document.getElementById('lat').textContent = myVariable;
+
+    //Values
+    document.getElementById("lat").textContent = lat.toFixed(4);
+    document.getElementById("lon").textContent = long.toFixed(4);
+    document.getElementById("alt").textContent = alt.toFixed(3);
+    document.getElementById("vel").textContent = velocity.toFixed(3);
+    document.getElementById("ave").textContent = (sum / velocityData.length).toFixed(3);
+    document.getElementById("max").textContent = (Math.max(...velocityData).toFixed(3));
+    document.getElementById("min").textContent = (Math.min(...velocityData).toFixed(3));
+
+    if (lat > 1 && long < 0) {
         document.getElementById('pos').textContent = "North-Western Hemisphere"
     }
-    else if (latitude > 1 && longitude > 0) {
+    else if (lat > 1 && long > 0) {
         document.getElementById('pos').textContent = "North-Eastern Hemisphere"
     }
-    else if (latitude < -1 && longitude < 0) {
+    else if (lat < -1 && long < 0) {
         document.getElementById('pos').textContent = "South-Western Hemisphere"
     }
-    else if (latitude < -1 && longitude > 0) {
+    else if (lat < -1 && long > 0) {
         document.getElementById('pos').textContent = "South-Eastern Hemisphere"
     }
-    else if (latitude >= -1.0 && latitude <= 1.0) {
+    else if (lat >= -1.0 && lat <= 1.0) {
         document.getElementById('pos').textContent = "Equator"
     }
 
-    if (visibility === "daylight") {
+    if (visibilityValue === "daylight") {
         document.getElementById('time').className = "fa-solid fa-sun"
     }
     else {
@@ -70,14 +78,35 @@ function locationISS(latitude, longitude, visibility) {
     }
 }
 
-function printAttributes(velocityData, latitude, longitude, altitude, velocity, sum) {
-    document.getElementById("lat").textContent = latitude.toFixed(4);
-    document.getElementById("lon").textContent = longitude.toFixed(4);
-    document.getElementById("alt").textContent = altitude.toFixed(3);
-    document.getElementById("vel").textContent = velocity.toFixed(3);
-    document.getElementById("ave").textContent = (sum / velocityData.length).toFixed(3);
-    document.getElementById("max").textContent = (Math.max(...velocityData).toFixed(3));
-    document.getElementById("min").textContent = (Math.min(...velocityData).toFixed(3));
+function switchToMap() {
+    L.tileLayer(tileUrl, { attribution }).addTo(map);
 }
+
+function switchToSatellite() {
+    L.tileLayer(
+        tileUrlSat, {
+        attribution: '&copy; ' + mapLink + ', ' + attributionSat,
+        maxZoom: 20,
+    }).addTo(map);
+}
+
+const api_url = "https://api.wheretheiss.at/v1/satellites/25544";
+const velocityData = [];
+let sum = 0;
+//Main
+async function getISS() {
+    const response = await fetch(api_url);
+    const data = await response.json();
+    const { latitude, longitude, altitude, velocity, visibility } = data;
+    sum += velocity;
+    velocityData.push(velocity);
+    marker.setLatLng([latitude, longitude]);
+    printAttributes(velocityData, latitude, longitude, altitude, velocity, sum, visibility);
+    setTimeout(getISS, 4000);
+}
+
+//Buttons
+document.getElementById('btn-satellite').addEventListener('click', switchToSatellite);
+document.getElementById('btn-map').addEventListener('click', switchToMap);
 
 getISS();
